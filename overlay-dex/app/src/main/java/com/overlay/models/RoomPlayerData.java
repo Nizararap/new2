6c10419a50d31e86513483130751a31e923166ba
Nizararap <nizarardhian8@gmail.com>
2026-06-22 18:25:04 +0700

    Delete overlay-dex/app/src/main/java/com/overlay/models/RoomPlayerData.java

--- overlay-dex/app/src/main/java/com/overlay/models/RoomPlayerData.java
+++
@@ -1,7 +0,0 @@
-package com.overlay.models;
-
-public class RoomPlayerData {
-    public int camp, uid, zone, heroId, rank, mythPt, matches, wins, accLv, spellId, countryId;
-    public boolean isLeader;
-    public String name;
-}
